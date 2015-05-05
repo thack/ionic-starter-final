891b22f3fcf5849b63f9cd08d2d49239b02622c0
Terence Carroll <git@thack.com>
2015-05-05 14:51:11 -0700

    Add tabs.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,44 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+angular.module('starter').config(function($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider.state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+    });
+
+    $stateProvider.state('tab.dash', {
+        url: '/dash',
+        views: {
+            'tab-first': {
+                templateUrl : 'templates/dash.html',
+                controller: 'DashCtrl'
+            }
+        }
+    });
+    
+    $stateProvider.state('tab.person', {
+        url: '/person/:personId',
+        views: {
+            'tab-first': {
+                templateUrl: 'templates/person.html',
+                controller: 'PersonCtrl'
+            }
+        }
+    });
+
+    $stateProvider.state('tab.simple', {
+        url: '/simple',
+        views: {
+            'tab-second': {
+                templateUrl: 'templates/simple.html'
+            }
+        }
+    });
+
+    $urlRouterProvider.otherwise('/tab/dash');
+
+});
--- www/js/services.js
+++ www/js/services.js
@@ -1,3 +1,4 @@
 // Simple example service that returns data
 angular.module('starter.services', []).factory('Friends', function() {
+    return {};
 });
