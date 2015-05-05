ae059922a97ea745caf9ce7ef63f90b0652eb563
Terence Carroll <git@thack.com>
2015-05-05 15:00:26 -0700

    Add dashboard.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,7 @@
 angular.module('starter.controllers', []);
 
 angular.module('starter.controllers').controller('DashCtrl', function($scope, Friends) {
+    $scope.people = Friends.all();
 });
 
 angular.module('starter.controllers').controller('PersonCtrl', function($scope, Friends, $stateParams) {
